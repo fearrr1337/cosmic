81440185ce8d397452ec2d9cb745815b0b42f6ab
fearrr1337 <fearren01@gmail.com>
2025-10-19 18:20:29 +0300

    Add namespace to SpaceShip interface

--- src/Base/SpaceShip.php
+++ src/Base/SpaceShip.php
@@ -1,9 +1,12 @@
 <?php
 
+namespace cosmic\src\Base;
+
 interface SpaceShip {
     public function fly(): void;
 
     public function shoot(): void;
 
     public function displayStatus(): void;
+
 }
